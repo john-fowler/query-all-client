8c1b7f82377ac060de0099cc0baaff723757ab51
John Fowler <john.fowler@jmrapidapps.net>
2024-05-24 12:42:52 -0500

    Forgot to add the changes to the Panes to accept heights.

--- src/components/ErrorPane.tsx
+++ src/components/ErrorPane.tsx
@@ -3,12 +3,15 @@ import { Paper, Typography, useTheme } from '@mui/material';
 
 interface ErrorPaneProps {
     error: string;
+    height: number;
 }
 
-const ErrorPane: React.FC<ErrorPaneProps> = ({ error }) => {
+const ErrorPane: React.FC<ErrorPaneProps> = ({ error, height }) => {
     const theme = useTheme();
     return (
-        <Paper elevation={3} style={{ padding: '10px' }}>
+        <Paper
+            elevation={3}
+            style={{ padding: '10px', height, overflowY: 'auto' }}>
             <Typography variant='h6' style={{ marginBottom: 20 }}>
                 SQL Error
             </Typography>
--- src/components/ExecutionPlan.tsx
+++ src/components/ExecutionPlan.tsx
@@ -3,11 +3,14 @@ import { Paper, Typography } from '@mui/material';
 
 interface ExecutionPlanProps {
     plan: string;
+    height: number;
 }
 
-const ExecutionPlan: React.FC<ExecutionPlanProps> = ({ plan }) => {
+const ExecutionPlan: React.FC<ExecutionPlanProps> = ({ plan, height }) => {
     return (
-        <Paper elevation={3} style={{ padding: '10px' }}>
+        <Paper
+            elevation={3}
+            style={{ padding: '10px', height, overflowY: 'auto' }}>
             <Typography variant='h6'>Execution Plan</Typography>
             <Typography
                 style={{
